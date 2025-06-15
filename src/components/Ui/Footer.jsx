356517c3c5530b1426827a74bb6a00c7531ec137
Gideon Julian <gideonchinonso77@gmail.com>
2025-06-15 18:31:12 +0100

    updated

--- src/components/Ui/Footer.jsx
+++ src/components/Ui/Footer.jsx
@@ -11,43 +11,43 @@ import AppleStore from "../../assets/icons/Apple.png";
 import playStore from "../../assets/icons/playStore.png";
 const Footer = () => {
   return (
-    <div className="w-full bg-[#1D2026] p-4 pt-20">
-      <div className="max-w-[1200px] mx-auto flex items-center justify-between">
+    <div className="w-full bg-[#1D2026] p-4 md:pt-20 pt-10">
+      <div className="max-w-[1200px] mx-auto  md:flex-row flex-col flex items-center justify-between">
         <div>
-          <h1 className="text-[#FFFFFF] text-3xl font-[600] w-[450px]">
+          <h1 className="text-[#FFFFFF] text-2xl md:text-3xl font-[600] md:w-[450px]">
             Start learning with 67.1k students around the world.
           </h1>
           <div className="flex items-center gap-5 mt-4 ">
             <Button
               style={
-                "text-[#fff] font-[500] px-6 py-3 bg-[#FF6636]  rounded-sm"
+                "text-[#fff] font-[500] px-4 md:px-6 py-3 bg-[#FF6636]  rounded-sm"
               }
               text={"Join the Family  "}
             />
             <Button
               style={
-                "text-[#fff] font-[500] px-6 py-3 bg-[#363B47]  rounded-sm"
+                "text-[#fff] font-[500] px-4 md:px-6 py-3 bg-[#363B47]  rounded-sm"
               }
               text={"Browse all courses  "}
             />
           </div>
         </div>
-        <div className="flex items-center gap-20 justify-between">
+        <div className="flex mt-3 items-center gap-10 md:gap-20 justify-between">
           <div>
-            <h1 className="font-[600] text-4xl text-white ">6.3k</h1>
+            <h1 className="font-[600] text-2xl md:text-4xl text-white ">6.3k</h1>
             <p className="text-[#B7BAC7] pt-2">Online courses</p>
           </div>
           <div>
-            <h1 className="font-[600] text-4xl text-white ">26kk</h1>
+            <h1 className="font-[600] text-2xl md:text-4xl text-white ">26k</h1>
             <p className="text-[#B7BAC7] pt-2">Certified Instructor</p>
           </div>
           <div>
-            <h1 className="font-[600] text-4xl text-white ">99.9%</h1>
+            <h1 className="font-[600] text-2xl md:text-4xl text-white ">99.9%</h1>
             <p className="text-[#B7BAC7] pt-2">Sucess Rate</p>
           </div>
         </div>
       </div>
-      <div className="max-w-[1200px] mx-auto flex items-center justify-between mt-30 pb-20">
+      <div className="max-w-[1200px] mx-auto flex md:flex-row flex-col items-center justify-between mt-10 md:mt-30 pb-20">
         <div>
           <img src={whitelogo} />
           <p className="text-[#8C94A3] w-[360px] mt-4">
@@ -62,7 +62,7 @@ const Footer = () => {
             <img src={YT} alt="" />
           </div>
         </div>
-        <div className="flex items-center gap-14">
+        <div className="md:flex grid grid-cols-2 md:items-center gap-5 md:gap-14 md:flex-row flex-col">
           <ul>
             <h1 className="text-[#ffff]">TOP 4 CATEGORY</h1>
             <li className="text-[#8C94A3] mt-4">Development</li>
@@ -96,7 +96,7 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="w-full p-4 border-t-2 border-[#363B47] ">
+      {/* <div className="w-full p-4 border-t-2 border-[#363B47] ">
         <div className="max-w-[1200px] mx-auto flex items-center justify-between">
           <div>
             <h1 className="text-[#8C94A3]">
@@ -106,10 +106,10 @@ const Footer = () => {
             </h1>
           </div>
           <div>
-            <input  />
+            <input />
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
