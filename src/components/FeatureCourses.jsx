8eb4332daa2eff96e0c9d7a9c2bcc52f4a01596c
Gideon Julian <gideonchinonso77@gmail.com>
2025-06-23 21:28:08 +0100

    updated

--- src/components/FeatureCourses.jsx
+++ src/components/FeatureCourses.jsx
@@ -1,74 +1,70 @@
 import React from "react";
+import FeaturesCourseCard from "./Ui/FeaturesCourseCard";
+import course1 from "../assets/images/course1.png";
+import course2 from "../assets/images/course2.png";
+import course3 from "../assets/images/course3.png";
+import course6 from "../assets/images/course6.png";
 import course7 from "../assets/images/course7.png";
-import star from "../assets/icons/Star.png";
-import clock from "../assets/icons/Clock.png";
-import User from "../assets/icons/User.png";
-import bars from "../assets/icons/bar-chart.png";
+
 const FeatureCourses = () => {
+  const data = [
+    {
+      courseName: "Investing In Stocks The Complete Course! (13 H...",
+      category: "Lifestyle",
+      price: 57,
+      rate: 5.0,
+      studentNumber: "265.7K",
+      img: course1,
+      bgcolor: "#E1F7E3",
+      textColor: "#15711F",
+    },
+    {
+      courseName: "Adobe XD for Web Design: Essential Principles",
+      nameColor: '#FF6636',
+      category: "Productivity",
+      price: 57,
+      rate: 5.0,
+      studentNumber: "265.7K",
+      img: course2,
+      bgcolor: "#F5F7FA",
+      textColor: "#1D2026",
+    },
+    {
+      courseName: "Google Analytics Certification - Learn How To...",
+      category: "Personal Development",
+      price: 57,
+      rate: 5.0,
+      studentNumber: "265.7K",
+      img: course6,
+      bgcolor: "#FFEEE8",
+      textColor: "#993D20",
+    },
+    {
+      courseName: "Investing In Stocks The Complete Course! (13 H...",
+      category: "Lifestyle",
+      price: 57,
+      rate: 5.0,
+      studentNumber: "265.7K",
+      img: course7,
+      bgcolor: "#FFF2E5",
+      textColor: "#",
+    },
+  ];
   return (
-    <div className="mx-auto max-w-[1290px]">
-      <div className="shadow-md p-5 ">
-        <div className="flex  items-center justify-between">
+    <div className="mx-auto max-w-[1390px]">
+      <div className="flex  items-center justify-between">
+        {" "}
+        <h1 className="text-2xl">Our Feature courses </h1>
+        <p className="text-[#4E5566] w-[450px]">
           {" "}
-          <h1 className="text-2xl">Our Feature courses </h1>
-          <p className="text-[#4E5566] w-[450px]">
-            {" "}
-            Vestibulum sed dolor sed diam mollis maximus vel nec dolor. Donec
-            varius purus et eleifend porta.
-          </p>
-        </div>
-        <div className="mt-5">
-          <div className="flex  items-center border border-[#E9EAF0] w-[650px]">
-            <div>
-              <img src={course7} className="h-[180px] object-cover" />
-            </div>
-            <div className=" w-full p-3">
-              <div className="flex items-center justify-between pb-4">
-                <div className="px-3 py-1 bg-[#E1F7E3]">
-                  <h1 className="text-[#15711F]">Health & Fitness</h1>
-                </div>
-                <div>
-                  <h1 className="text-lg">$14.00</h1>
-                </div>
-              </div>
-              <h1 className="text-[#1D2026] font-bold">
-                Investing In Stocks The Complete Course! (13 H...
-              </h1>
-              <div className="mt-3 flex justify-between items-center">
-                <h1 className="text-[#4E5566] ">Kevin Gilbert</h1>
-                <div className="flex items-center">
-                  <img src={star} className="w-6 h-6" alt="" />
-                  <h1 className="text-[#4E5566] font-bold text-lg">
-                    5.0{" "}
-                    <span className="text-[#8C94A3] font-normal">
-                      (357,914)
-                    </span>
-                  </h1>
-                </div>
-              </div>
-              <div className="flex items-center justify-around mt-4">
-                <div className="flex items-center gap-2">
-                  <img src={User} alt="" />
-                  <h1 className="text-[#4E5566] font-bold text-lg">
-                    265.7K
-                    <span className="text-[#8C94A3] font-normal">students</span>
-                  </h1>
-                </div>{" "}
-                <div className="flex items-center gap-2">
-                  <img src={bars} alt="" />
-                  <h1 className="text-[#4E5566] font-bold text-lg">
-                    Beginners
-                  </h1>
-                </div>    <div className="flex items-center gap-2">
-                  <img src={clock} alt="" />
-                  <h1 className="text-[#4E5566] font-bold text-lg">
-                  6 hours 
-                  </h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+          Vestibulum sed dolor sed diam mollis maximus vel nec dolor. Donec
+          varius purus et eleifend porta.
+        </p>
+      </div>
+      <div className="shadow-md p-5 grid grid-cols-2 gap-10">
+        {data.map((items) => (
+          <FeaturesCourseCard img={items.img} category={items.category} nameColor={items.nameColor} bgColor={items.bgcolor} textColor={items.textColor} courseName={items.courseName} />
+        ))}
       </div>
     </div>
   );
--- src/components/Ui/FeaturesCourseCard.jsx
+++ src/components/Ui/FeaturesCourseCard.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
-
-const FeaturesCourseCard = () => {
+import React from "react";
+import course7 from '../../assets/images/course7.png'
+import star from "../../assets/icons/Star.png";
+import clock from "../../assets/icons/Clock.png";
+import User from "../../assets/icons/User.png";
+import bars from "../../assets/icons/bar-chart.png";
+const FeaturesCourseCard = ({img,textColor,courseName, bgColor,nameColor,category}) => {
   return (
-    <div>FeaturesCourseCard</div>
-  )
-}
+    <div>
+     
+      <div className="mt-5">
+        <div className="flex  items-center border border-[#E9EAF0] w-[650px]">
+          <div>
+            <img src={img} className="h-[180px] object-cover" />
+          </div>
+          <div className=" w-full p-3">
+            <div className="flex items-center justify-between pb-4">
+              <div className="px-3 py-1 bg-[]" style={{backgroundColor: bgColor}}>
+                <h1 className="" style={{ color: textColor}}>{category}</h1>
+              </div>
+              <div>
+                <h1 className="text-lg">$14.00</h1>
+              </div>
+            </div>
+            <h1 className="font-bold" style={{color: nameColor}}>
+              {courseName}
+            </h1>
+            <div className="mt-3 flex justify-between items-center">
+              <h1 className="text-[#4E5566] ">Kevin Gilbert</h1>
+              <div className="flex items-center">
+                <img src={star} className="w-6 h-6" alt="" />
+                <h1 className="text-[#4E5566] font-bold text-lg">
+                  5.0{" "}
+                  <span className="text-[#8C94A3] font-normal">(357,914)</span>
+                </h1>
+              </div>
+            </div>
+            <div className="flex items-center justify-around mt-4">
+              <div className="flex items-center gap-2">
+                <img src={User} alt="" />
+                <h1 className="text-[#4E5566] font-bold text-lg">
+                  265.7K
+                  <span className="text-[#8C94A3] font-normal">students</span>
+                </h1>
+              </div>{" "}
+              <div className="flex items-center gap-2">
+                <img src={bars} alt="" />
+                <h1 className="text-[#4E5566] font-bold text-lg">Beginners</h1>
+              </div>{" "}
+              <div className="flex items-center gap-2">
+                <img src={clock} alt="" />
+                <h1 className="text-[#4E5566] font-bold text-lg">6 hours</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FeaturesCourseCard
+export default FeaturesCourseCard;
